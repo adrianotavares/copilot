9ee8fb138db76f958d04ff35673cdbb3be4233f4
Adriano Tavares <adriano.tavares@gmail.com>
2024-07-29 18:09:24 -0300

    Update comments

--- script.py
+++ script.py
@@ -4,15 +4,8 @@ from removebg import RemoveBg
 from PIL import Image
 
 def remove_background(input_file):
-    """
-    Remove the background from an image file using the RemoveBg library.
+    # Remove the background from an image file using the RemoveBg library.
 
-    Parameters:
-    input_file (str): The path to the input image file.
-    
-    Returns:
-    None
-    """
     try:
         rmbg = RemoveBg("k7tsZ6E5h3jXEyTLfigbMNB", "error.log")
         rmbg.remove_background_from_img_file(input_file)
@@ -28,16 +21,8 @@ def remove_background(input_file):
 
 # new function to resize the image receive the arguments image path, -r, the new size
 def resize_image(input_file, new_size):
-    """
-    Resize the image file using the PIL library.
+    #Resize the image file using the PIL library.
 
-    Parameters:
-    input_file (str): The path to the input image file.
-    new_size (str): The new size of the image file.
-    
-    Returns:
-    None
-    """
     try:
         img = Image.open(input_file)
         resized_img = img.resize(new_size)
@@ -48,9 +33,8 @@ def resize_image(input_file, new_size):
         print("An error occurred", e)
         
 def main():
-    """
-    Parse command-line arguments and call the appropriate function.
-    """
+    #Parse command-line arguments and call the appropriate function.
+    
     parser = argparse.ArgumentParser(description='Remove background from image.')
     parser.add_argument('input_file', type=str, help='The image file to process.')
     parser.add_argument('-rb', action='store_true', help='Remove background from image.')
